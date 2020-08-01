d16e2e766cd8448e2717de9a43fc7fe34145c581
Gautam Sahu <gautamsahu770@gmail.com>
2020-08-01 14:05:39 +0530

    4th Commit
    In this commit i have done upto Designing and show all the videos and details to the user.

--- Movie_Site_IMDB/form.py
+++ Movie_Site_IMDB/form.py
@@ -5,7 +5,7 @@ class UserRegistrationForm(forms.ModelForm):
     name=forms.CharField(label="Name",widget=forms.TextInput(attrs={'pattern':'^[a-zA-Z " " ]*$'}))
     email=forms.EmailField(label="Email")
     contactno=forms.IntegerField(label="Contact No.",widget=forms.NumberInput(attrs={'pattern':'^[7-9]{1}[0-9]{9}$'}))
-    password=forms.CharField(label="Password")
+    password=forms.CharField(label="Password",widget=forms.PasswordInput)
     class Meta:
         fields="__all__"
         model=UserRegistrationModel
